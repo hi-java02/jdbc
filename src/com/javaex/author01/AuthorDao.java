cf27727ce17a17457e1c20efa3ced8b9cbdf7407
hi <hi@DESKTOP-DF0PRAE>
2020-12-29 13:06:46 +0900

    author delete 기능 추가

--- src/com/javaex/author01/AuthorDao.java
+++ src/com/javaex/author01/AuthorDao.java
@@ -18,6 +18,128 @@ public class AuthorDao {
 	// 메소드 g/s
 
 	// 메소드 일반
+	// 작가 수정하기
+	public int authorUpdate(int authorId, String name, String desc ) {
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		int count = 0;
+		
+		try {
+		    // 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+		    // 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+		    // 3. SQL문 준비 / 바인딩 / 실행
+			String query ="";
+			query += " update author ";
+			query += " set author_name = ? , ";
+			query += "     author_desc = ? ";
+			query += " where author_id = ? ";
+			System.out.println(query);
+			
+			pstmt = conn.prepareStatement(query);  //쿼리로 만들기
+			pstmt.setString(1, name);
+			pstmt.setString(2, desc);
+			pstmt.setInt(3, authorId);
+			
+			count = pstmt.executeUpdate();
+		    
+			// 4.결과처리
+			System.out.println("[dao] " + count + " 건 수정");
+			
+
+		} catch (ClassNotFoundException e) {
+		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		} finally {
+		   
+		    // 5. 자원정리
+		    try {
+		        if (pstmt != null) {
+		            pstmt.close();
+		        }
+		        if (conn != null) {
+		            conn.close();
+		        }
+		    } catch (SQLException e) {
+		        System.out.println("error:" + e);
+		    }
+
+		}
+		
+		return count;
+	}
+	
+	
+	
+	
+	// 작가 삭제하기
+	public int authorDelete(int authorId ) {
+		
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		int count = 0;
+		
+		try {
+		    // 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+		    // 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+		    // 3. SQL문 준비 / 바인딩 / 실행
+			String query ="";
+			query += " delete from author "; 
+			query += " where author_id = ? ";
+			
+			//테스트
+			System.out.println(query);
+			
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, authorId);
+			
+			count = pstmt.executeUpdate();
+			
+		    // 4.결과처리
+			System.out.println("[dao]" + count + "건 삭제");
+			
+
+		} catch (ClassNotFoundException e) {
+		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		} finally {
+		   
+		    // 5. 자원정리
+		    try {           
+		        if (pstmt != null) {
+		            pstmt.close();
+		        }
+		        if (conn != null) {
+		            conn.close();
+		        }
+		    } catch (SQLException e) {
+		        System.out.println("error:" + e);
+		    }
+
+		}
+		
+		return count;
+	}
+	
+	
+	
+	
+	
 	// 작가 리스트 가져오기
 	public List<AuthorVo> getAuthorList() {
 
